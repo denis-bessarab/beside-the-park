e7e613050cd98666c2fba9c43c34ea0eef7a3716
Denis Bessarab <99955098+Shmiterling@users.noreply.github.com>
2023-06-06 01:03:03 +0200

    Header component
    Added Header Component. Imported all the fonts, provided basic scss variables and global styling parameters. Made header responsible on desktop and mobile devices.

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,24 @@
+import React, {useEffect, useState} from 'react';
+import './header.scss';
+
+function Header() : React.JSX.Element {
+
+	const [screenSize,setScreenSize] = useState<number>(window.innerWidth)
+
+	const handleResize = () : void => {
+		setScreenSize(window.innerWidth)
+	}
+
+	useEffect(() => {
+		window.addEventListener('resize',handleResize)
+	},[])
+
+	return (
+		<header id="header">
+			<h1>{(screenSize >= 768) ? "LOREM" : "LOREM IPSUM"}</h1>
+			{(screenSize >= 768) && <div className="blue-fill"></div>}
+		</header>
+	);
+}
+
+export default Header;
