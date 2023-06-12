b64dc10d960993227bd24da65347ff18b8b65b59
Denis Bessarab <99955098+Shmiterling@users.noreply.github.com>
2023-06-12 23:14:08 +0200

    Loading and Error state of request
    Provided handling of loading and error state for apollo request in EpisodesList.tsx component. Added file loader.gif. Provided style corrections.

--- src/components/EpisodesList/EpisodesList.tsx
+++ src/components/EpisodesList/EpisodesList.tsx
@@ -3,12 +3,15 @@ import "./episodesList.scss";
 import {EpisodeInterface} from "../../interfaces/interfaces";
 import {useFourthSeasonEpisodes} from "../../api/hooks";
 import Episode from "../Episode/Episode";
+import loader from '../../assets/loader.gif'
 function EpisodesList() : React.JSX.Element {
 
 	const {error, data, loading} = useFourthSeasonEpisodes();
 	
 	return (
 		<ul className="Episodes-List" data-testid="episodes-list">
+			{loading && <li className="loader"><img src={loader} alt="loader"/></li>}
+			{error && <li className="error">Oops... Something's gone wrong :(</li>}
 			{data && data.episodes.results.map((episode:EpisodeInterface,index:number) => {
 				return <Episode key={index} air_date={episode.air_date} episode={episode.episode} name={episode.name} __typename={episode.__typename}/>
 			})}
